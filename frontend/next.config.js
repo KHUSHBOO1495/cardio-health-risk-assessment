17830aebfb97f32c3aa4981be26b4382e5de1935
KHUSHBOO1495 <lokhushboo1409@gmail.com>
2026-01-15 15:05:16 +0530

    Initially changes for frontend deployment

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,12 +1,6 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   reactStrictMode: true,
-// }
-
-// module.exports = nextConfig
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "standalone",
+  output: "export",
 };
 
 module.exports = nextConfig;
